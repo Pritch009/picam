e62f127a7e5feb8d152d3d71f04592b715266975
Pritch009 <will@pritch.net>
2025-04-03 21:20:28 -0400

    Fixing pi camera selection

--- rich_camera.py
+++ rich_camera.py
@@ -14,7 +14,7 @@ if use_mock_camera:
     print("Using MockCamera")
 else:
     try:
-        from pi_camera import HWCamera
+        from pi_camera import Camera as HWCamera
         print("Using Raspberry Pi camera")
     except ImportError:
         print("picamera not found.  Using MockCamera.  Set environment variable USE_MOCK_CAMERA=TRUE to suppress this message.")
